e187789f86a6c58afcda49558b618eb314434e31
ALightInTheDark <2713120362@qq.com>
2021-02-18 11:17:11 +0800

    Site updated: 2021-02-18 11:17:11

--- js/title.js
+++ js/title.js
@@ -1,15 +1,15 @@
-// 浏览器标题
+// 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         document.querySelector('[rel="icon"]').setAttribute('href', "/img/funny.ico");
-        document.title = '看不见我看不见我~~~';
+        document.title = '(っ °Д °;)っ 访问的页面不存在了';
         clearTimeout(titleTime);
     }
     else {
         document.querySelector('[rel="icon"]').setAttribute('href', "/img/favicon.ico");
-        document.title = '欢迎回来！' + OriginTitle;
+        document.title = '(●\'◡\'●)噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
